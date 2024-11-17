cae251050356c6f334f36d7ae4b1e798caa9540f
Marc <mailnawman@gmail.com>
2024-11-17 18:41:46 +0100

    Add qr factorization

--- ten/kernels/lapack_api.hxx
+++ ten/kernels/lapack_api.hxx
@@ -1,35 +1,64 @@
-#ifndef TEN_KERNELS_BLAS_API_HXX
-#define TEN_KERNELS_BLAS_API_HXX
+#ifndef TEN_KERNELS_LAPACK_API
+#define TEN_KERNELS_LAPACK_API
 
 #include <cstddef>
+
+#include <ten/types.hxx>
 
 #ifdef __APPLE__
 #include <Accelerate/Accelerate.h>
 #else
-#include <lapack/lapack.h>
+#include <lapacke.h>
 #endif
 
 namespace ten::kernels::lapack {
 
 enum class transop : char { no = 'N', trans = 'T' };
 
-static LAPACK_TRANSPOSE cast(const transop op) {
+static CBLAS_TRANSPOSE cast(const transop op) {
    if (op == transop::no) {
-      return LAPACK_TRANSPOSE::LapackNoTrans;
+      return CBLAS_TRANSPOSE::CblasNoTrans;
    } else {
-      return LAPACK_TRANSPOSE::LapackTrans;
+      return CBLAS_TRANSPOSE::CblasTrans;
+   }
+}
+
+static int cast(const storage_order order) {
+   if(order == storage_order::col_major) {
+      return LAPACK_COL_MAJOR;
+   } else {
+      return LAPACK_ROW_MAJOR;
    }
 }
 
 /// QR factorization
 template <typename T>
-static void qrFactorization();
+static void qr_fact(storage_order layout, size_t m, size_t n, T* a, size_t lda, T* tau);
 
 template<>
-static void qrFactorization() {
-
+static void qr_fact(storage_order layout, size_t m, size_t n, float* a, size_t lda, float* tau) {
+   LAPACKE_sgeqrf(cast(layout), m, n, a, lda, tau);
 };
 
-} // namespace ten::kernels::blas
+template<typename T>
+static void qr_factq(storage_order layout, size_t m, size_t n, size_t k, T* a, size_t lda, T* tau);
+
+template<>
+static void qr_factq(storage_order layout, size_t m, size_t n, size_t k, float* a, size_t lda, float* tau) {
+   LAPACKE_sorgqr(cast(layout), m, n, k, a, lda, tau);
+}
+
+// LU factorization
+template<typename T>
+static void lu_fact(storage_order layout, size_t m, size_t n, T* a, size_t lda, int* ipiv);
+
+template<>
+static void lu_fact(storage_order layout, size_t m, size_t n, float* a, size_t lda, int* ipiv) {
+   LAPACKE_sgetrf(cast(layout), m, n, a, lda, ipiv);
+}
+
+// TODO Cholesky factorization
+
+} // namespace ten::kernels::lapack
 
 #endif
--- ten/linear_algebra/factorization.hxx
+++ ten/linear_algebra/factorization.hxx
@@ -1,35 +1,93 @@
 #ifndef TENSEUR_LINEAR_ALGEBRA_FACTORIZATION
 #define TENSEUR_LINEAR_ALGEBRA_FACTORIZATION
 
-#include <Ten/Types.hxx>
+#include <ten/kernels/lapack_api.hxx>
+#include <ten/tensor.hxx>
 
-namespace ten{
+namespace ten {
 
-class QR{
-private:
-   Matrix<float> _q;
-   Matrix<float> _r;
+template <class __t = float> class qr {
+ private:
+   matrix<__t> _q;
+   matrix<__t> _r;
 
-public:
-   QR() {};
+ public:
+   qr(){};
 
-   void factorize(const Matrix<float>& a) {
-      ::ten::kernels::qrFactorization(a, q, r);
+   void factorize(const matrix<__t> &x) {
+      // Copy x
+      matrix<__t> a(x.shape());
+      for (size_t i = 0; i < x.dim(0); i++) {
+         for (size_t j = 0; j < x.dim(1); j++) {
+            a(i, j) = x(i, j);
+         }
+      }
+
+      size_t m = a.dim(0);
+      size_t n = a.dim(1);
+      auto layout = a.storage_order();
+      _q = ::ten::zeros<matrix<__t>>({m, n});
+      _r = ::ten::zeros<matrix<__t>>({n, n});
+
+      matrix<__t> tau({n});
+      size_t lda = a.is_transposed() ? n : m;
+      ::ten::kernels::lapack::qr_fact(layout, m, n, a.data(), lda, tau.data());
+      // Copy R
+      for (size_t i = 0; i < n; i++) {
+         for (size_t j = i; j < n; j++) {
+            _r(i, j) = a(i, j);
+         }
+      }
+      // Copy Q
+      size_t k = n;
+      ::ten::kernels::lapack::qr_factq(layout, m, n, k, a.data(), lda,
+                                       tau.data());
+      for (size_t i = 0; i < n; i++) {
+         for (size_t j = 0; j < n; j++) {
+            _q(i, j) = a(i, j);
+         }
+      }
    }
 
-   auto q() const {return _q;}
+   auto q() const { return _q; }
 
-   auto r() const {return _r;}
-
-   auto qr() const {return std::make_tuple(_q, _r);}
+   auto r() const { return _r; }
 };
 
-// TODO LU
+// TODO LU factorization
+template <class __t = float> class lu {
+ private:
+   matrix<__t> __l;
+   matrix<__t> __u;
 
-// TODO SVD
+ public:
+   void factorize(matrix<__t> a) {}
 
-// TODO Cholesky
+   auto l() const { return __l; }
 
-}
+   auto u() const { return __u; }
+};
+
+// TODO SVD factorization
+template <class __t = float> class svd {
+ private:
+   matrix<__t> __u;
+   diagonal<__t> __sigma;
+   matrix<__t> __vt;
+
+ public:
+   void factorize(matrix<__t> a) {}
+
+   auto u() const { return __u; }
+
+   auto sigma() const { return __sigma; }
+
+   auto vt() const { return __vt; }
+};
+
+// Cholesky
+template <class __t = float> class cholesky {};
+
+} // namespace ten
 
 #endif
