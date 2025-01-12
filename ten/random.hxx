0455cd006182ea4ba4267bc1eea817226687a24e
Marc <mailnawman@gmail.com>
2025-01-12 20:41:13 +0100

    Fix seed in random.hxx

--- ten/random.hxx
+++ ten/random.hxx
@@ -11,26 +11,29 @@
 namespace ten {
 /// Random tensor
 /// rand<tensor<...>, ...>(shape)
-template <DynamicTensor __t, class __distribution = uniform<typename __t::value_type>>
+template <DynamicTensor __t, class __distribution = std::uniform_real_distribution<typename __t::value_type>>
 auto rand(std::initializer_list<size_t> &&dims) {
    __t x(std::move(dims));
    __distribution dist;
+   std::random_device rd;
+   std::mt19937 gen(rd());
    // Fill with random data
    for (size_t i = 0; i < x.size(); i++) {
-      x[i] = dist.sample();
+      x[i] = dist(gen);
    }
    return x;
 }
 
 /// Random tensor with seed
 /// rand<tensor<...>, ...>(shape)
-template <DynamicTensor __t, class __distribution = uniform<typename __t::value_type>>
+template <DynamicTensor __t, class __distribution = std::uniform_real_distribution<typename __t::value_type>>
 auto rand(std::initializer_list<size_t> &&dims, size_t seed) {
    __t x(std::move(dims));
-   __distribution dist(seed);
+   __distribution dist;
+   std::mt19937 gen(seed);
    // Fill with random data
    for (size_t i = 0; i < x.size(); i++) {
-      x[i] = dist.sample();
+      x[i] = dist(gen);
    }
    return x;
 }
@@ -48,26 +51,29 @@ auto rand(std::initializer_list<size_t> &&dims) {
 
 /// Random static tensor
 /// rand<stensor<...>>
-template <StaticTensor __t, class __distribution = uniform<typename __t::value_type>>
+template <StaticTensor __t, class __distribution = std::uniform_real_distribution<typename __t::value_type>>
 auto rand() {
    __t x;
    __distribution dist;
+   std::random_device rd;
+   std::mt19937 gen(rd());
    // Fill with random data
    for (size_t i = 0; i < x.size(); i++) {
-      x[i] = dist.sample();
+      x[i] = dist(gen);
    }
    return x;
 }
 
 /// Random static tensor with seed
 /// rand<stensor<...>>
-template <StaticTensor __t, class __distribution = uniform<typename __t::value_type>>
+template <StaticTensor __t, class __distribution = std::uniform_real_distribution<typename __t::value_type>>
 auto rand(size_t seed) {
    __t x;
-   __distribution dist(seed);
+   __distribution dist;
+   std::mt19937 gen(seed);
    // Fill with random data
    for (size_t i = 0; i < x.size(); i++) {
-      x[i] = dist.sample();
+      x[i] = dist(gen);
    }
    return x;
 }
