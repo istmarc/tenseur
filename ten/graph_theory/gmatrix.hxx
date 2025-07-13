6d25f847b03ea618a8e7ca3e53ddb746ac78c7c9
Marc <mnawman@proton.me>
2025-07-13 13:13:07 +0200

    Graph adjacency matrix

--- ten/graph_theory/gmatrix.hxx
+++ ten/graph_theory/gmatrix.hxx
@@ -7,7 +7,7 @@
 namespace ten{
 namespace graph{
 
-template<class T = bool>
+template<class T = float>
 class gmatrix{
    private:
       size_t _vertices;
@@ -38,54 +38,6 @@ class gmatrix{
 template<class T>
 std::ostream &operator<<(std::ostream& os, const gmatrix<T>& g) {
    os << g._m;
-   return os;
-}
-
-template<>
-class gmatrix<bool> {
-   private:
-      size_t _vertices;
-      std::vector<bool> _m;
-      graph_type _gtype;
-
-   size_t get_index(size_t i, size_t j) const {
-      return i * _vertices + j;
-   }
-
-   public:
-      gmatrix(const size_t vertices,const graph_type gtype = graph_type::undirected): 
-         _vertices(vertices), _gtype(gtype) {
-         _m = std::vector<bool>(vertices * vertices * vertices, false);
-      };
-
-   void add_edge(size_t src, size_t dest) {
-      size_t idx = get_index(src, dest);
-      _m[idx] = true;
-      if (_gtype == graph_type::undirected) {
-         size_t idx = get_index(dest, src);
-         _m[idx] = true;
-      }
-   }
-
-   bool has_edge(size_t src, size_t dest) const {
-      size_t idx = get_index(src, dest);
-      return _m[idx];
-   }
-
-   template<class _T>
-   friend std::ostream &operator<<(std::ostream&, const gmatrix<_T>& );
-};
-
-template<>
-std::ostream &operator<<(std::ostream& os, const gmatrix<bool>& g) {
-   os << "matrix<bool," << g._vertices << "x" << g._vertices << ">\n";
-   for (size_t i = 0; i < g._vertices; i++) {
-      os << g.has_edge(i, 0);
-      for (size_t j = 1; j < g._vertices; j++) {
-         os << "   " << g.has_edge(i, j);
-      }
-      os << "\n";
-   }
    return os;
 }
 
