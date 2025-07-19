dcaad91eaa4685aab0fbbf735a7c64e046812be4
Marc <mnawman@proton.me>
2025-07-19 14:14:34 +0200

    Weighted adjacency list

--- ten/graphs/gweighted.hxx
+++ ten/graphs/gweighted.hxx
@@ -3,62 +3,73 @@
 
 #include <ten/tensor>
 
-#include <ten/graphs/types.hxx>
 #include <map>
+#include <ten/graphs/types.hxx>
 
 namespace ten {
 namespace graph {
 
 /// Graph as a weighted ajanceny list
-template<class T = float>
-class gweighted {
+template <class T, class W = float> class gweighted {
  private:
    ten::graph::graph_type _type;
-   std::map<size_t, std::vector<std::pair<size_t, T>>> _graph;
+   std::map<T, std::vector<std::pair<T, W>>> _graph;
 
  public:
-   gweighted(ten::graph::graph_type graph_type = ten::graph::graph_type::undirected)
+   gweighted(
+       ten::graph::graph_type graph_type = ten::graph::graph_type::undirected)
        : _type(graph_type) {}
 
    bool empty() { return _graph.empty(); }
 
-   void add_vertex(size_t vertex) { _graph[vertex] = std::vector<std::pair<size_t, T>>(); }
+   void add_vertex(const T &vertex) {
+      _graph[vertex] = std::vector<std::pair<T, W>>();
+   }
 
-   void add_edge(size_t src, size_t dest, T weight) {
+   void add_edge(const T &src, const T &dest, W weight) {
       _graph[src].push_back(std::make_pair(dest, weight));
       if (_type == graph_type::undirected) {
          _graph[dest].push_back(std::make_pair(src, weight));
       }
    }
 
-   bool has_edge(size_t src, size_t dest) {
+   bool has_edge(const T &src, const T &dest) {
       auto it = _graph[src].begin();
       while (it < _graph[src].end()) {
          auto [d, w] = *it;
-         if (d == dest) return true;
+         if (d == dest)
+            return true;
          it++;
       }
       return false;
    }
 
    // Get the weight of the edge src->dest
-   T weight(size_t src, size_t dest) {
+   T weight(const T &src, const T &dest) {
       auto it = _graph[src].begin();
       while (it < _graph[src].end()) {
          auto [d, w] = *it;
-         if (d == dest) return w;
+         if (d == dest)
+            return w;
          it++;
       }
       return T(0);
    }
 
    // Get the adjacency matrix
-   auto matrix() -> ten::matrix<T> {
+   auto matrix() -> ::ten::matrix<W> {
       size_t n = _graph.size();
-      ten::matrix<float> m({n, n});
-      for (size_t i = 0; i < n; i++) {
-         for (size_t j = 0; j < n; j++) {
-            m(i, j) = weight(i, j);
+      ::ten::matrix<W> m = ::ten::zeros<::ten::matrix<W>>({n, n});
+      std::map<T, size_t> map;
+      size_t i = 0;
+      for (auto it = _graph.begin(); it != _graph.end(); it++) {
+         map[it->first] = i;
+         i++;
+      }
+
+      for (auto const &[src, value] : _graph) {
+         for (auto const &[dest, weight] : value) {
+            m(map[src], map[dest]) = weight;
          }
       }
       return m;
