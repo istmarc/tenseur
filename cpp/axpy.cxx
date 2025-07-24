c3171b0721e6be11c68d45d378f973a704fc180f
Marc <mnawman@proton.me>
2025-07-24 16:55:37 +0200

    axpy eample

---
+++ cpp/axpy.cxx
@@ -0,0 +1,13 @@
+#include <ten/tensor>
+
+int main() {
+   auto x = ten::range<ten::vector<float>>({10});
+   auto y = ten::range<ten::vector<float>>({10});
+
+   ten::axpy(2.0f, x, y);
+   std::cout << y << std::endl;
+
+   auto z = 2.0f * x;
+   ten::axpy(2.0f, z, y);
+   std::cout << y << std::endl;
+}
