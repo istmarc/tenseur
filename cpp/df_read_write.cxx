5f578024c62be7a0356355f1fda230b4d6ca1314
Marc <mnawman@proton.me>
2026-01-11 17:06:21 +0000

    Test for data frame write to csv

---
+++ cpp/df_read_write.cxx
@@ -0,0 +1,18 @@
+#include <ten/tensor>
+#include <ten/dataframe>
+
+int main() {
+   ten::dataframe df;
+
+   ten::vector<float> x({3}, {1.0f, 2.0f, 3.0f});
+   std::vector<std::string> y({"A", "B", "C"});
+   ten::vector<double> z({3}, {-1.0, -2.0, -3.0});
+
+   df.add_col("x", x);
+   df.add_col("y", y);
+   df.add_col("z", z);
+
+   save(df, "dataframe.csv");
+
+}
+
