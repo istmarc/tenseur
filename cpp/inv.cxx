2e3ce1b2f5ea0ade7ae02afca549a0197a80ea0d
Marc <mnawman@proton.me>
2026-01-14 17:16:07 +0000

    Update inverse of a matrix

--- cpp/inv.cxx
+++ cpp/inv.cxx
@@ -1,11 +1,12 @@
 #include <ten/tensor>
 #include <ten/linalg>
+#include <ten/io>
 
 int main() {
    ten::matrix<float> x({3, 3}, {1., 0., 1., 0., -1., -2., 2., 1., 0.});
 
    std::cout << x << std::endl;
-   auto y = ten::inv(x);
+   auto y = ten::linalg::inv(x);
    std::cout << y << std::endl;
 
    std::cout << (x * y).eval() << std::endl;
