cc0c083a10bbeb56a2f551f88d54d9a44c4e1605
Marc <mnawman@proton.me>
2026-02-27 20:11:20 +0000

    Fix failing random test

--- tests/Tensor/Random.hxx
+++ tests/Tensor/Random.hxx
@@ -1,18 +1,21 @@
 #ifndef TENSEUR_TESTS_TENSOR_RANDOM
 #define TENSEUR_TESTS_TENSOR_RANDOM
 
+#include "ten/distributions.hxx"
 #include <ten/tensor>
 #include <ten/tests.hxx>
 #include <ten/random.hxx>
 
-/// FIXME This test fails
 TEST(Random, RandomFloatTensor) {
    using namespace ten;
 
    size_t seed = 1234;
-   auto x = ten::rand_norm<ten::smatrix<float, 2, 3>>(seed);
-   auto y = ten::rand_norm<ten::matrix<float>>({2, 3}, seed);
-   auto z = ten::rand_norm<ten::tensor<float, 3>>({2, 3, 4}, seed);
+   ten::set_seed(seed);
+   auto x = ten::rand_norm<ten::smatrix<float, 2, 3>>();
+   ten::set_seed(seed);
+   auto y = ten::rand_norm<ten::matrix<float>>({2, 3});
+   ten::set_seed(seed);
+   auto z = ten::rand_norm<ten::tensor<float, 2>>({2, 3});
 
    ASSERT_TRUE(tests::same_values(x, y));
 
