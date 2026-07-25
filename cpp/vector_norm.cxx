10559eeca95c82a09b218ae5fd56681a9811eb0e
Marc <mnawman@proton.me>
2026-07-25 18:22:18 +0100

    Update vector norm example

--- cpp/vector_norm.cxx
+++ cpp/vector_norm.cxx
@@ -1,4 +1,5 @@
 #include <ten/tensor>
+#include <ten/io>
 #include <ten/linalg>
 
 int main() {
@@ -7,9 +8,10 @@ int main() {
    std::cout << v << std::endl;
 
    std::cout << "Norms" << std::endl;
-   std::cout << ten::norm(v, ten::vector_norm::l1) << std::endl;
-   std::cout << ten::norm(v, ten::vector_norm::l2) << std::endl;
-   std::cout << ten::norm(v, 3) << std::endl;
-   std::cout << ten::norm(v, 4) << std::endl;
-   std::cout << ten::norm(v, ten::vector_norm::linf) << std::endl;
+   std::cout << ten::linalg::norm(v, ten::linalg::vector_norm::l1) << std::endl;
+   std::cout << ten::linalg::norm(v, ten::linalg::vector_norm::l2) << std::endl;
+   std::cout << ten::linalg::pnorm(v, 3) << std::endl;
+   std::cout << ten::linalg::pnorm(v, 4) << std::endl;
+   std::cout << ten::linalg::norm(v, ten::linalg::vector_norm::linf) << std::endl;
 }
+
